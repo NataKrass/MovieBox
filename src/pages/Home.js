8aada3044f7574629df5c32a312779d5a33cbfad
Nata <natalikrasnobaieva@gmail.com>
2020-12-14 21:47:35 +0200

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MainPageLayout } from '../components/MainPageLayout'
+
+const Home = () => {
+    return (
+        <MainPageLayout>
+            This is home page
+        </MainPageLayout>
+    )
+}
+
+export default Home
---
+++ src/pages/Starred.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MainPageLayout } from '../components/MainPageLayout'
+
+const Starred = () => {
+    return (
+        <MainPageLayout>
+            This is starred page
+        </MainPageLayout>
+    )
+}
+
+export default Starred
